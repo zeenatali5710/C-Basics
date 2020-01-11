aef44ad431adecbd77f7a0820d9fa71fa200b70f
zeenatali5710 <55951660+zeenatali5710@users.noreply.github.com>
2020-01-10 20:51:23 -0400

    Add files via upload

---
+++ LinkedList.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : LearningC.c
+ Author      : Zeenat Ali
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Linked List, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
